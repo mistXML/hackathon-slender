7df28d7906d4a88fb333a428201221f41101939e
Abigail <abby.w.gail@gmail.com>
2025-10-25 21:52:59 +0100

    Slenderman distance shader works

--- Scripts/Slenderman/slenderman.gd
+++ Scripts/Slenderman/slenderman.gd
@@ -2,13 +2,14 @@ extends CharacterBody3D
 
 @onready var nav_agent = $NavAgent/NavigationAgent3D
 @onready var player = get_tree().get_current_scene().get_node("Player")
+@onready var color_rect = get_tree().get_current_scene().get_node("CanvasLayer/ColorRect")
+
 var next_position : Vector3
 const speed = 2.5
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
@@ -17,3 +18,24 @@ func _physics_process(delta: float) -> void:
 	next_position = nav_agent.get_next_path_position()
 	velocity = global_position.direction_to(next_position) * speed
 	move_and_slide()
+
+var max_distance := 10.0 #we can adjust the required distance and stuff
+var min_transparency := 0.0
+var max_transparency := 8.0
+
+func _process(delta):
+	var dist = global_position.distance_to(player.global_position)
+	var t = clamp(dist / max_distance, 0.0, 1.0)
+	var current_transparency = lerp(min_transparency, max_transparency, t)
+	
+	var mat = color_rect.material
+	if mat and mat is ShaderMaterial:
+		mat.set_shader_parameter("Transparency", current_transparency)
+		
+		
+		
+		
+		
+		
+		
+		
